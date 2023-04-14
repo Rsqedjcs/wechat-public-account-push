266095b7151f556e59d6a69ad0248a3ff2b87475
谢灵运气超好 <50611554+Rsqedjcs@users.noreply.github.com>
2023-04-14 19:30:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs'
 import { selfDayjs, timeZone } from './src/utils/set-def-dayjs.js'
-import { getAccessToken, getWeather,getCIBA,
+import { getAccessToken, getWeather3,getCIBA,
     getOneTalk, getBirthdayMessage, sendMessageReply,
     callbackReply, 
     getDateDiffList,
@@ -17,18 +17,18 @@ const main = async () => {
     const province = config.PROVINCE
     const city = config.CITY
     // 获取每日天气
-//     const {
-//         // 天气
-//         weather,
-//         // 最高温度
-//         temp: maxTemperature, 
-//         // 最低温度
-//         tempn: minTemperature,
-//         // 风向
-//         wd: windDirection,
-//         // 风力等级
-//         ws: windScale
-//     } = await getWeather(province, city)
+    const {
+        // 天气
+        weather,
+        // 最高温度
+        temp: maxTemperature, 
+        // 最低温度
+        tempn: minTemperature,
+        // 风向
+        wd: windDirection,
+        // 风力等级
+        ws: windScale
+    } = await getWeather3(province, city)
     // 获取金山词霸每日一句
     const { content: noteEn, note: noteCh} = await getCIBA()
     // 获取每日一言
